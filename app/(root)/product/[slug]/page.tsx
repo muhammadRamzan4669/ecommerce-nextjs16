dbc397a347f9197060257cd8463f4f3380d530d1
LYNX <ramzanlynx@outlook.com>
2025-12-04 23:03:08 +0500

    Product Details Page added

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,8 +1,12 @@
 import { getProductBySlug } from "@/lib/actions/product.actions";
+import { notFound } from "next/navigation";
 
 export default async function ProductDetailsPage(props: { params: Promise<{ slug: string }> }) {
   const { slug } = await props.params;
   const product = await getProductBySlug(slug)
+
+  if (!product)
+    notFound();
 
   return <>{product.name}</>
 }
