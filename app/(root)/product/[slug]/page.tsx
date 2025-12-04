1c949a575c261931a61ded45f720d45b1145b63c
LYNX <ramzanlynx@outlook.com>
2025-12-04 22:59:25 +0500

    Product Details Page added

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,5 +1,8 @@
+import { getProductBySlug } from "@/lib/actions/product.actions";
+
 export default async function ProductDetailsPage(props: { params: Promise<{ slug: string }> }) {
   const { slug } = await props.params;
+  const product = await getProductBySlug(slug)
 
-  return <>Product Details Page</>
+  return <>{product.name}</>
 }
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,9 +3,15 @@ import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({
-    take: LATEST_PRODUCTS_LIMIT, 
+    take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' }
   });
 
   return data;
+}
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug }
+  })
 }
